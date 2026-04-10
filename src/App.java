97bafbfcaac8ef5033bd6dc78516a0cf3a1f9a3b
BryanM112 <estbryanmc@gmail.com>
2026-04-10 12:52:35 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int nu =10;
+        int [] array =new int[] {1,2,6,70,5};
+        System.out.println(nu);
+        System.out.println(Arrays.toString(array));
+        for(int i= 0; i<array.length;i++){
+            System.out.println(i);
+        }
+        for(int i : array){
+            System.out.println(i);
+        }
     }
 }
